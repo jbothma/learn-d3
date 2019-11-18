b3792a71405a8588c67e4e02d64066872b329584
JD Bothma <jbothma@gmail.com>
2019-11-18 11:19:38 +0200

    Buggy but dynamic data and axes

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ function drawChart() {
   // append the svg object to the body of the page
   // append a 'group' element to 'svg'
   // moves the 'group' element to the top left margin
-  var svg = d3.select("body").append("svg")
+  var svg = d3.select("#root").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
@@ -26,30 +26,47 @@ function drawChart() {
             "translate(" + margin.left + "," + margin.top + ")");
 
 
-  const data = generateData();
 
-  // Scale the range of the data in the domains
-  x.domain(data.map(function(d) { return d.name; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+  function showData(data) {
+    const nodes = svg.selectAll(".bar");
 
-  // append the rectangles for the bar chart
-  svg.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) { return x(d.name); })
-    .attr("width", x.bandwidth())
-    .attr("y", function(d) { return y(d.value); })
-    .attr("height", function(d) { return height - y(d.value); });
+    // Scale the range of the data in the domains
+    x.domain(data.map(function(d) { return d.name; }));
+    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+    // append the rectangles for the bar chart
+
+    nodes.data(data)
+      .enter().append("rect")
+      .attr("class", "bar");
+
+    nodes
+      .attr("x", function(d) { return x(d.name); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .exit().remove();
+
+    svg.selectAll(".x-axis").call(d3.axisBottom(x));
+    svg.selectAll(".y-axis").call(d3.axisLeft(y));
+  }
 
   // add the x Axis
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .attr("class", "x-axis");
+
 
   // add the y Axis
   svg.append("g")
-    .call(d3.axisLeft(y));
+    .attr("class", "y-axis");
+
+
+  d3.select("#update-data").on("click", function() {
+    showData(generateData());
+  });
+
+  showData(generateData());
 }
 
 
